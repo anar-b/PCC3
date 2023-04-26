f8c351e7881578fccb85a8e5aefb7dd352ff85c2
anar-b <barazandehanar@gmail.com>
2023-04-26 11:06:44 -0700

    added to players.py

--- players.py
+++ players.py
@@ -4,4 +4,14 @@ players = ["telma",
            "asal",
            "mahsa",]
 
-print(players[-3:])
+# print(players[1:])
+# print(players[1:3])
+# print(players[-3:])
+# print(players[:4]) 
+
+
+
+
+print("here are the first three players on my team:")
+for player in players [:3]:
+    print(player.title())
