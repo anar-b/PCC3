4e5a2b060b08b9811b5883a2ada944464bec6580
anar-b <barazandehanar@gmail.com>
2023-04-27 07:39:40 -0700

    foods.py

---
+++ foods.py
@@ -0,0 +1,6 @@
+my_foods = ["burger","fried chicken","french fries","kebab","white soup","pizza",] 
+friend_foods = my_foods[:]
+print("these are my favorite foods:")
+print(my_foods)
+print("\n And these are my bestie's favorite foods:")
+print(friend_foods)
