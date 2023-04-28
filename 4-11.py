9a8386c880ae2bd00d1b41b1cd9267483079f692
anar-b <barazandehanar@gmail.com>
2023-04-28 03:49:09 -0700

    4-11 solved

--- 4-11.py
+++ 4-11.py
@@ -1,9 +1,13 @@
-favorite_pissas = ["margarita", "pepperoni", "italian"]
-friend_pizza = favorite_pissas[:]
+favorite_pizzas = ["margarita", "pepperoni", "italian"]
+friend_pizzas = favorite_pizzas[:]
 
-favorite_pissas.append("special")
-friend_pizza.apprnd("vegies")  
+favorite_pizzas.append("special")
+friend_pizzas.append("vegies")  
 
-print("my favorite pizza are:")
-for food in favorite_pissas:
-    print(f{pizza}s")
+print("my favorite pizzas are:")
+for pizza in favorite_pizzas:
+    print(f"-{pizza}")
+    
+print("\nMy friend's favorite pizzas are:")
+for pizza in friend_pizzas:
+    print(f"-{pizza}")
