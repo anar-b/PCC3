c1fc98ded5dd1d34c44eb38d6669290222f575e3
anar-b <130584988+anar-b@users.noreply.github.com>
2023-05-07 23:22:33 +0430

    solved 4-13

---
+++ 4-13.py
@@ -0,0 +1,16 @@
+basic_foods = (
+    "fried egg",
+    "fried chicken",
+    "burger",
+    "salad",
+    "omlet"
+)
+
+basic_foods[0] = "french fries"
+
+for food in basic_foods:
+   print(
+       "we serve: ",
+       food,
+       "for dinner"
+   )
